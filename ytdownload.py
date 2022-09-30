095755d4b45d96cf69a2334f9f44e031878d2e50
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2022-09-30 05:41:32 +0530

    Update ytdownload.py

--- ytdownload.py
+++ ytdownload.py
@@ -1,10 +1,10 @@
 from pytube import YouTube
 import title
-print(":")
+print("Please input your youtube link below:")
 link = input(">")
 print("")
 yt = YouTube(link)
-ys = yt.streams.highest_resolution()
+ys = yt.streams.get_highest_resolution()
 print("Video title: " + yt.title)
 print("Resolution: " + ys.resolution)
 ys.download("")
